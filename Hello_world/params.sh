0cd4d9246f0e732f09acde3a608390d8d06f532d
pmpardeshi <pramodmpardeshi@gmail.com>
2020-07-23 11:28:22 +0530

    adding folders

--- Hello_world/params.sh
+++ Hello_world/params.sh
@@ -9,7 +9,10 @@ exit 0
 
 #exit same as return, 0 means sucess everything other is error
 #exit code range is 0-255
+
 #output
+#$ ./params.sh Pramod
 #hello Pramod
 #Sun Jul 19 09:49:20 IST 2020
 #/home/pmpardeshi/pypractice/bash/shell scripting
+#$
